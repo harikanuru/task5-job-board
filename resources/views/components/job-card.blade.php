85689e758f8cc79c03f1e17a26205497268a7205
HariKrishna <vanapamula.harikrishna@gmail.com>
2023-09-29 07:51:39 +0530

    Added filtering to the jobsj

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -16,7 +16,7 @@
 
                 </div>
             </div>
-            <p class = 'text-sm text-slate-500'>{!! nl2br(e($job->description)) !!}</p>
+            
 
            {{ $slot }}
         </x-card>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <x-job-card :$job />
+   <x-breadcrumbs  class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#' ]" />
+    <x-job-card :$job >
+        <p class = 'text-sm text-slate-500'>{!! nl2br(e($job->description)) !!}</p>
+    </x-job-card>
 </x-layout>
