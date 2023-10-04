e63ce6fc558f179902c8e4430a5d6b1df0ab86a2
HariKrishna <vanapamula.harikrishna@gmail.com>
2023-10-04 11:47:28 +0530

    Added code

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -10,7 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Employer extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['company_name'];
+    
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
